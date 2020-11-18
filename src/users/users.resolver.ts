38500457e4ab41a94536e3d88a09c07a8039d1a6
md2eoseo <md2eoseo@gmail.com>
2020-11-18 15:13:52 +0900

    createAccount, login

---
+++ src/users/users.resolver.ts
@@ -0,0 +1,43 @@
+import { Args, Mutation, Query } from '@nestjs/graphql';
+import { Resolver } from '@nestjs/graphql';
+import {
+  CreateAccountInput,
+  CreateAccountOutput,
+} from './dtos/create-account.dto';
+import { LoginInput, LoginOutput } from './dtos/login.dto';
+import { User } from './entities/user.entity';
+import { UsersService } from './users.service';
+
+@Resolver((of) => User)
+export class UsersResolver {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Query((returns) => Boolean)
+  hi() {
+    return true;
+  }
+
+  @Mutation((returns) => CreateAccountOutput)
+  async createAccount(
+    @Args('input') createAccountInput: CreateAccountInput,
+  ): Promise<CreateAccountOutput> {
+    try {
+      const { ok, error } = await this.usersService.createAccount(
+        createAccountInput,
+      );
+      return { ok, error };
+    } catch (error) {
+      return { ok: false, error };
+    }
+  }
+
+  @Mutation((returns) => LoginOutput)
+  async login(@Args('input') loginInput: LoginInput): Promise<LoginOutput> {
+    try {
+      const { ok, error, token } = await this.usersService.login(loginInput);
+      return { ok, error, token };
+    } catch (error) {
+      return { ok: false, error };
+    }
+  }
+}
