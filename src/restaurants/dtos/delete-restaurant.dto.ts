1e9f5ba675e9488878385588a7887e10ab34654e
md2eoseo <md2eoseo@gmail.com>
2020-11-21 14:26:55 +0900

    deleteRestaurant

---
+++ src/restaurants/dtos/delete-restaurant.dto.ts
@@ -0,0 +1,9 @@
+import { Field, InputType, ObjectType, PickType } from '@nestjs/graphql';
+import { CoreOutput } from 'src/common/dtos/output.dto';
+import { Restaurant } from '../entities/restaurant.entity';
+
+@InputType()
+export class DeleteRestaurantInput extends PickType(Restaurant, ['id']) {}
+
+@ObjectType()
+export class DeleteRestaurantOutput extends CoreOutput {}
--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -7,6 +7,10 @@ import {
   CreateRestaurantOutput,
 } from './dtos/create-restaurant.dto';
 import {
+  DeleteRestaurantInput,
+  DeleteRestaurantOutput,
+} from './dtos/delete-restaurant.dto';
+import {
   EditRestaurantInput,
   EditRestaurantOutput,
 } from './dtos/edit-restaurant.dto';
@@ -47,7 +51,6 @@ export class RestaurantService {
     try {
       const restaurant = await this.restaurants.findOne(
         editRestaurantInput.restaurantId,
-        { loadRelationIds: true },
       );
       if (!restaurant) {
         return { ok: false, error: 'Restaurant not found' };
@@ -71,6 +74,28 @@ export class RestaurantService {
       return { ok: true };
     } catch (e) {
       return { ok: false, error: 'Could not edit restaurant' };
+    }
+  }
+
+  async deleteRestaurant(
+    owner: User,
+    { id }: DeleteRestaurantInput,
+  ): Promise<DeleteRestaurantOutput> {
+    try {
+      const restaurant = await this.restaurants.findOne(id);
+      if (!restaurant) {
+        return { ok: false, error: 'Restaurant not found' };
+      }
+      if (owner.id !== restaurant.ownerId) {
+        return {
+          ok: false,
+          error: "You can't delete restaurant you don't own",
+        };
+      }
+      await this.restaurants.delete(id);
+      return { ok: true };
+    } catch (e) {
+      return { ok: false, error: 'Could not delete restaurant' };
     }
   }
 }
