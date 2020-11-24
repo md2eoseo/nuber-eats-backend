7ed9bc9e46b9e8bb66fa008aafe037046a9b2a12
md2eoseo <md2eoseo@gmail.com>
2020-11-24 16:55:38 +0900

    payments resolver, service

---
+++ src/payments/dtos/get-payments.dto.ts
@@ -0,0 +1,12 @@
+import { Field, InputType, ObjectType } from '@nestjs/graphql';
+import { CoreOutput } from 'src/common/dtos/output.dto';
+import { Payment } from '../entities/payment.entity';
+
+@InputType()
+export class GetPaymentsInput {}
+
+@ObjectType()
+export class GetPaymentsOutput extends CoreOutput {
+  @Field((type) => [Payment], { nullable: true })
+  payments?: Payment[];
+}
