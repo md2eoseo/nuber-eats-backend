09571a9a399eef6d744eb418c065178c597556e2
md2eoseo <md2eoseo@gmail.com>
2020-12-11 11:34:40 +0900

    fixed how to make new category name

--- src/restaurants/repositories/category.repository.ts
+++ src/restaurants/repositories/category.repository.ts
@@ -4,10 +4,13 @@ import { Category } from '../entities/category.entity';
 @EntityRepository(Category)
 export class CategoryRepository extends Repository<Category> {
   async getOrCreate(name: string): Promise<Category> {
-    const categoryName = name.trim().toLowerCase();
-    const categorySlug = categoryName.replace(/ /g, '-');
+    const cleanedCategoryName = name.trim().toLowerCase();
+    const categorySlug = cleanedCategoryName.replace(/ /g, '-');
     let category = await this.findOne({ slug: categorySlug });
     if (!category) {
+      const categoryName =
+        cleanedCategoryName.charAt(0).toUpperCase() +
+        cleanedCategoryName.slice(1);
       category = await this.save(
         this.create({ name: categoryName, slug: categorySlug }),
       );
