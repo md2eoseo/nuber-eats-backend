2366ab610eeb0c572c419ad415672ae8ab5a5993
md2eoseo <md2eoseo@gmail.com>
2020-12-13 16:42:19 +0900

    uploads controller with aws s3

--- src/app.module.ts
+++ src/app.module.ts
@@ -20,6 +20,7 @@ import { OrderItem } from './orders/entities/order-item.entity';
 import { CommonModule } from './common/common.module';
 import { PaymentsModule } from './payments/payments.module';
 import { Payment } from './payments/entities/payment.entity';
+import { UploadsModule } from './uploads/uploads.module';
 
 @Module({
   imports: [
@@ -84,6 +85,7 @@ import { Payment } from './payments/entities/payment.entity';
     OrdersModule,
     CommonModule,
     PaymentsModule,
+    UploadsModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/uploads/uploads.controller.ts
@@ -0,0 +1,40 @@
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import * as AWS from 'aws-sdk';
+
+const BUCKET_NAME = '2eoseonubereats';
+
+@Controller('uploads')
+export class UploadsController {
+  @Post('')
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() file) {
+    AWS.config.update({
+      credentials: {
+        accessKeyId: process.env.AWS_S3_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_S3_SECRET_KEY,
+      },
+    });
+    try {
+      const objectName = `${Date.now() + file.originalname}`;
+      await new AWS.S3()
+        // .createBucket({ Bucket: BUCKET_NAME })
+        .putObject({
+          Body: file.buffer,
+          Bucket: BUCKET_NAME,
+          Key: objectName,
+          ACL: 'public-read',
+        })
+        .promise();
+      const url = `https://${BUCKET_NAME}.s3.amazonaws.com/${objectName}`;
+      return { url };
+    } catch (e) {
+      console.log(e);
+    }
+  }
+}
